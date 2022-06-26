f7816c7b4c2c9bc87d2c6b0c222595d0efbde614
Huỳnh Quang Phúc <quangphuc.huynh95@gmail.com>
2022-06-26 16:56:40 +0700

    fix some problems

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build:{
+    outDir: './public/admin-frontend'
+  },
+  publicDir: './admin-frontend/public',
+  server: {
+    proxy: {
+      '/admin-api': 'http://localhost:8000/admin-api'
+    }
+  }
 })
